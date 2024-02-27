28463c621a75b563dceb15cedcd4eab01d4dc280
doppant <nnoormansyah@gmail.com>
2024-02-28 06:03:53 +0700

    tugas 7: halaman(users, usersDetail), routing(link header)

--- sanber_nextjs/components/header/index.js
+++ sanber_nextjs/components/header/index.js
@@ -1,8 +1,21 @@
 import { withAuth } from "../with-auth"
 import styles from "./styles.module.css"
+import Link from "next/link";
 
 function Header() {
-  return <div className={styles.header}>header</div>
+  return <div className={styles.header}>
+    <ul>
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <li>
+        <Link href="/profil">Profil</Link>
+      </li>
+      <li>
+        <Link href="/users">Users</Link>
+      </li>
+    </ul>
+  </div>
 }
 
 export default withAuth(Header);
